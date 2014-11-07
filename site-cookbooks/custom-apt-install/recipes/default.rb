7fb0b1e42df87bf9f18f7a91075f2b882e8362b7
phrase5036 <phrase5036@gmail.com>
2014-11-07 11:34:51 +0800

    set nginx user & fix custom apt

--- site-cookbooks/custom-apt-install/recipes/default.rb
+++ site-cookbooks/custom-apt-install/recipes/default.rb
@@ -7,29 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 execute 'apt-get update' do
-  command 'sudo apt-get update'
+  user 'root'
+  command 'apt-get update'
 end
 
 execute 'apt-get ntpdate' do
-  command 'sudo apt-get install -y ntpdate'
+  user 'root'
+  command 'apt-get install -y ntpdate'
 end
 
 execute 'apt-get build-essential bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev libxslt1.1 libxslt1-dev libxml2 libcurl4-openssl-dev libapr1-dev libaprutil1-dev autoconf automake libtool pkg-config' do
-  command 'sudo apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev libxslt1.1 libxslt1-dev libxml2 libcurl4-openssl-dev libapr1-dev libaprutil1-dev autoconf automake libtool pkg-config'
+  user 'root'
+  command 'apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev libxslt1.1 libxslt1-dev libxml2 libcurl4-openssl-dev libapr1-dev libaprutil1-dev autoconf automake libtool pkg-config'
 end
 
 execute 'apt-get mysql-client libmysql++-dev' do
-  command 'sudo apt-get install -y mysql-client libmysql++-dev'
+  user 'root'
+  command 'apt-get install -y mysql-client libmysql++-dev'
 end
 
 execute 'apt-get git-core vim' do
-  command 'sudo apt-get install -y git-core vim'
+  user 'root'
+  command 'apt-get install -y git-core vim'
 end
 
 execute 'apt-get imagemagick graphicsmagick-libmagick-dev-compat' do
-  command 'sudo apt-get install -y imagemagick graphicsmagick-libmagick-dev-compat'
+  user 'root'
+  command 'apt-get install -y imagemagick graphicsmagick-libmagick-dev-compat'
 end
 
 execute 'apt-get libmagickcore-dev libmagickwand-dev' do
-  command 'sudo apt-get install -y libmagickcore-dev libmagickwand-dev'
+  user 'root'
+  command 'apt-get install -y libmagickcore-dev libmagickwand-dev'
 end
